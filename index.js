36e41c09e9ad20ca3456e6b888ee72c71c953820
thusharke <thushar.ke25@gmail.com>
2020-08-02 10:59:47 +0530

    added nav bar

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var express  = require('express');
 var app = express();
 
+app.set("view engine","ejs");
+app.use(express.static(__dirname+"/public"));
+
 app.get("/",function(req,res){
-    res.send("Welcome to My brand new home page");
+    res.render("home");
 })
 
 app.listen(process.env.PORT||"3000",process.env.ID,function(){
