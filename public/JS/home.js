571d79adff0e158f7ee043a26bae13222df4f7ab
thusharke <thushar.ke25@gmail.com>
2020-08-02 22:32:01 +0530

    Made Animations

--- public/JS/home.js
+++ public/JS/home.js
@@ -1,6 +1,8 @@
 var preloader = document.querySelector("#preloader");
 var nav = document.querySelector("nav");
+var navtxt = document.querySelector(".nav-container")
 var Text =  document.querySelector("h1");
+var btn = document.querySelector(".new");
 var strText = Text.textContent;
 var splitText = strText.split("");
 console.log(splitText);
@@ -38,15 +40,21 @@ function wave(){
     },1000)
     setTimeout(function(){
         preloader.style.display = 'none';
-    },1500)
+        nav.style.opacity = 1;
+        btn.classList.add('animate');
+    },2000)
 }
 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    nav.style.backgroundColor = "rgb(0,0,0,0.8)";
+    nav.style.backgroundColor = "rgba(250,250,250,0.6)";
+    navtxt.style.color = 'black';
+    nav.classList.add('shadow');
   } else {
-    nav.style.backgroundColor = "rgb(0,0,0,0.3)";
+    nav.style.backgroundColor = "rgba(0,0,0,0.3)";
+    navtxt.style.color = 'white';
+    nav.classList.remove('shadow');
   }
 }
